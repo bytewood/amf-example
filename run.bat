750140195d1fff9000cbca0f982bbe6eda7fcfe5
bytewood <peter.wood.za@gmail.com>
2013-11-13 19:22:07 +0200

    Added an nginx project, flex client now deploys to nginx and talks via SSL through nginx to 2 different backend servers. also via SSL

--- run.bat
+++ run.bat
@@ -10,5 +10,18 @@ cd java-amf-two
 start mvn tomcat7:run
 
 cd ..
-cygstart "https://localhost:8443/java-amf/Main.html"
+cd nginx-manager\target\nginx-1.5.6\conf
+call openssl.bat
+cd ..
+start nginx.exe
+
+
+"c:\Program Files\Internet Explorer\iexplore.exe" "https://localhost/amf-client/Main.html"
+
+pause
+
+nginx -s stop
+
+cd ..
+cd ..
 
